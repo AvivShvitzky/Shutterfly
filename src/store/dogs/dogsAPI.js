3dabef01f647db055484969bf179883cf21c5d68
AvivShvitzky <asifaviv@zahav.net.il>
2022-04-19 05:25:13 +0300

    ADD promise all

--- src/store/dogs/dogsAPI.js
+++ src/store/dogs/dogsAPI.js
@@ -1,13 +1,14 @@
 /* Urls */
 const urls = {
   static: {
-    getAllDogs: 'https://dog.ceo/api/breeds/list/all',
-    getPlaceholderImage: 'https://via.placeholder.com/100'
+    getAllDogs: "https://dog.ceo/api/breeds/list/all",
+    getPlaceholderImage: "https://via.placeholder.com/100",
   },
   dynamic: {
-    getDogsImages: (breed) => `https://dog.ceo/api/breed/${breed}/images/random`
-  }
-} 
+    getDogsImages: (breed) =>
+      `https://dog.ceo/api/breed/${breed}/images/random`,
+  },
+};
 
 /* Actions */
 export function fetchDogs() {
@@ -19,10 +20,13 @@ export function fetchDogs() {
 export async function fetchDogsImages(dogs) {
   const mutatedDogs = JSON.parse(JSON.stringify(dogs));
 
-  for await (const breed of Object.keys(dogs)) {
-    await fetch(urls.dynamic.getDogsImages(breed))
-    .then((response) => response.json())
-    .then((data) => (mutatedDogs[breed].image = data.message));
-  }
+  const promises = Object.keys(dogs).map((breed) => {
+    return fetch(urls.dynamic.getDogsImages(breed))
+      .then((response) => response.json())
+      .then((data) => (mutatedDogs[breed].image = data.message));
+  });
+
+  await Promise.all(promises);
+
   return mutatedDogs;
 }
