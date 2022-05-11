71a4f4af08f07fe226fa2eac83e28c662d6780d6
AvivShvitzky <asifaviv@zahav.net.il>
2022-05-11 14:12:58 +0300

    delete unused files

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import restaurantReducer from "./restaurant/restaurantSlice";
-
-export const store = configureStore({
-  reducer: {
-    restaurant: restaurantReducer,
-  },
-});
